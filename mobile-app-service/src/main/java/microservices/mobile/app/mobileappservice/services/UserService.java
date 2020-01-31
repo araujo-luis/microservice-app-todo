c7abfcae782009860dcb7337fca3574cb89c847d
Luis Araujo <luis_araujocaballero@hotmail.com>
2020-01-31 17:34:04 -0600

    Format

--- mobile-app-service/src/main/java/microservices/mobile/app/mobileappservice/services/UserService.java
+++ mobile-app-service/src/main/java/microservices/mobile/app/mobileappservice/services/UserService.java
@@ -9,12 +9,12 @@ import microservices.mobile.app.mobileappservice.models.Users;
 public interface UserService {
 
 	public List<Users> getUsers();
-	
+
 	public ResponseEntity<Users> getUser(String userId);
-	
+
 	public ResponseEntity<Users> createUser(Users user);
-	
+
 	public ResponseEntity<Users> updateUser(String userId, Users user);
-	
+
 	public String deleteUser(String userId);
 }
