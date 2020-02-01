459d9ce7533840fc886bc9e9ee5ec7e0da2b8c2e
Luis Araujo <luis_araujocaballero@hotmail.com>
2020-01-31 20:06:59 -0600

    Eureka Server Discovery

---
+++ eureka-server-discovery/src/test/java/microservices/app/eurekaserverdiscovery/EurekaServerDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package microservices.app.eurekaserverdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
