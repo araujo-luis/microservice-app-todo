f81705cebd665995f0cccd8c4f251220dca20e32
Luis Araujo <luis_araujocaballero@hotmail.com>
2020-02-17 14:20:36 -0600

    Refactor config server

--- authentication-service/src/main/java/microservices/app/authenticationservice/controllers/AuthenticationController.java
+++ authentication-service/src/main/java/microservices/app/authenticationservice/controllers/AuthenticationController.java
@@ -1,19 +1,24 @@
 package microservices.app.authenticationservice.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/login")
+@RequestMapping("/authentication")
 public class AuthenticationController {
 
 	
 
-	@GetMapping()
-	public String hello() {
-		return "Hello";
+	@Autowired
+	private Environment env;
+
+	@GetMapping("/check/status")
+	public String checkStatus(){
+		return "Authentication Service is working on " + env.getProperty("local.server.port") + " Gateway " +env.getProperty("gateway.ip");
 	}
 
 }
